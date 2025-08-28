f6ad338db5a4e328e1c833fb835f6e6bb0259bc9
Ahmed.M Youssef <ahmed.muhammed.youssef@gmail.com>
2025-08-28 09:48:06 +0300

    feat: add user get by id endpoint

---
+++ src/HabitTracker.Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using HabitTracker.Api.Database;
+using HabitTracker.Api.DTOs.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HabitTracker.Api.Controllers;
+
+[ApiController]
+[Route("users")]
+internal sealed class UserController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetById(string id)
+    {
+        UserDto? userDto = await dbContext.Users.
+            Where(u => u.Id == id)
+            .Select(UserQueries.ProjectToDto())
+            .FirstOrDefaultAsync();
+
+        if (userDto == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(userDto);
+    }
+
+}
---
+++ src/HabitTracker.Api/DTOs/Users/UserQueries.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using HabitTracker.Api.Entities;
+
+namespace HabitTracker.Api.DTOs.Users;
+
+internal static class UserQueries
+{
+    public static Expression<Func<User, UserDto>> ProjectToDto()
+    {
+        return u => new UserDto
+        {
+            Id = u.Id,
+            Name = u.Name,
+            Email = u.Email,
+            CreatedAtUtc = u.CreatedAtUtc,
+            UpdatedAtUtc = u.UpdatedAtUtc
+        };
+    }
+}
